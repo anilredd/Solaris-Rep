b00b8f0a6efa4e99f03d263d3f13269c3fe1bd3a
vivek <vivekreddy080@gmail.com>
2018-10-10 18:24:29 +0530

    new file added

--- vivek.java
+++ vivek.java
@@ -1,1 +1,12 @@
-Amazon Elasticsearch Service, is a fully managed service that makes it easy for you to deploy, secure, operate, and scale Elasticsearch to search, analyze, and visualize data in real-time. With Amazon Elasticsearch Service you get easy-to-use APIs and real-time analytics capabilities to power use-cases such as log analytics, full-text search, application monitoring, and clickstream analytics, with enterprise-grade availability, scalability, and security. The service offers integrations with open-source tools like Kibana and Logstash for data ingestion and visualization. 
+Amazon Elasticsearch Service, is a fully managed service 
+that makes it easy for you to deploy, secure, 
+     operate, and scale Elasticsearch to search, 
+     analyze, and visualize data in real-time. With 
+     Amazon Elasticsearch Service you get easy-to-use 
+     APIs and real-time analytics capabilities to power 
+     use-cases such as log analytics, full-text search, 
+     application monitoring, and clickstream analytics,
+     with enterprise-grade availability, scalability, 
+     and security. The service offers integrations with 
+     open-source tools like Kibana and Logstash for 
+     data ingestion and visualization. 
